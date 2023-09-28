e5bc27a1a1892a8819b0445177f34a51d7192437
Cha Seoyeon <seoyeon0833@gmail.com>
2023-09-28 23:17:51 +0900

    Set player jumping
    Add codes for player jumping to the PlayerMovement script.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviour
     SpriteRenderer spriteRenderer;
     Animator anim;
     public float MaxSpeed;
+    public float jumpSpeed;
 
     private void Awake()
     {
@@ -18,13 +19,21 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        //Player jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping")) {
+            rigid.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
         //Stop player moving
         if (Input.GetButtonUp("Horizontal"))
             rigid.velocity = new Vector2(0.5f * rigid.velocity.normalized.x, rigid.velocity.y);
 
+        //Direction of sprite
         if (Input.GetButtonDown("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
 
+        //Changing animation parameter while player is walking
         if (Mathf.Abs(rigid.velocity.x) < 0.3)
             anim.SetBool("isWalking", false);
         else
@@ -43,5 +52,15 @@ public class PlayerMove : MonoBehaviour
             rigid.velocity = new Vector2(MaxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x <= MaxSpeed * (-1))   //Left Max Speed
             rigid.velocity = new Vector2(MaxSpeed * (-1), rigid.velocity.y);
+
+        //Raycast for stopping player jumping animation
+        if (rigid.velocity.y < 0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null && rayHit.distance < 0.5f)
+            {
+                anim.SetBool("isJumping", false);
+            }
+        }
     }
 }
