0ced1878f7135885f7b8c5b4484e4b84c233b7bf
Cha Seoyeon <seoyeon0833@gmail.com>
2023-10-03 17:49:43 +0900

    Create enemy AI
    Create enemy AI(Walk on the platform in a random direction).

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    public int nextMove;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Invoke("Think", 3);
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector2.down, 1, LayerMask.GetMask("Platform"));
+
+        if (rayHit.collider == null) {
+            nextMove *= -1;
+            spriteRenderer.flipX = nextMove == 1;
+
+            CancelInvoke();
+            Invoke("Think", 2);
+        }
+    }
+
+    private void Think() {
+        nextMove = Random.Range(-1, 2);
+
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        if (nextMove != 0) spriteRenderer.flipX = nextMove == 1;
+
+        float thinkTime = Random.Range(2f, 6f);
+        Invoke("Think", thinkTime);
+    }
+}
